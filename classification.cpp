16c3c15609f31eef095ff2fd905fcfb14efb00c5
Alexander Voß <a_voss19@uni-muenster.de>
2016-03-02 20:45:37 +0100

    sicherung

--- classification.cpp
+++ classification.cpp
@@ -6,33 +6,38 @@
 #include "binarizewolfjolion.h"
 
 using namespace std;
-using namespace cv;
 using namespace cv::text;
 
-Classification::Classification(const Mat& image, string filename) : originalImage(image), filename(filename)
+Classification::Classification(const cv::Mat& image, string filename) : originalImage(image), filename(filename)
 {
 
 }
 
 
-void Classification::characterRecognition(const Mat& image){
-    Segmentation segmentation(image, filename);
-    segmentation.cropImage(image);
+void Classification::characterRecognition(const cv::Mat& image){
+    MSER mser(image);
+    vector<cv::Rect> plates = mser.run();
+/*    cv::Mat plate = image(plates.at(0));
+    cout << plates.size() << endl;
+    imshow("plate", plate);
+
+   /* Segmentation segmentation(plate, filename);
+    segmentation.cropImage(plate);
     vector<cv::Mat> chars = Segmentation_MSER::findChars(segmentation.croppedImage);
     //segmentation.findChars();
     //vector<cv::Mat> chars = segmentation.chars;
 
-    Ptr<OCRTesseract> tesseract = OCRTesseract::create("/usr/local/share/tessdata", "deu2", NULL, 0, 10);
+    cv::Ptr<OCRTesseract> tesseract = OCRTesseract::create("/usr/local/share/tessdata", "deu2", NULL, 0, 10);
 
     string result = "";
     for(int i = 0; i < chars.size(); i++){
-        Mat character = chars.at(i);
+        cv::Mat character = chars.at(i);
 
         //Mat binchar = segmentation.computeBinaryImage(character, WOLFJOLION, 30);
 
-        //imshow(to_string(i), binchar);
+        cv::imshow(to_string(i), character);
         string output;
-        vector<Rect> boxes;
+        vector<cv::Rect> boxes;
         vector<string> words;
         vector<float> confidences;
         tesseract->run(character, output, &boxes, &words, &confidences);
